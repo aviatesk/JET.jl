c66ca0e31705450a7388fd704f835976918f7724
aviatesk <aviatesk@gmail.com>
2020-01-06 17:34:04 +0900

    simpler macros

--- src/profile.jl
+++ src/profile.jl
@@ -7,18 +7,15 @@ Adds `report` to `frame.reports` and then returns `Unknown` type away
 !!! note
     `frame::Frame` is supposed to exist in a local scope.
 """
-macro report!(report)
-  quote
-    report!(frame, $report)
-    return Unknown
-  end |> esc
-end
+macro report!(report) esc(:(report!(frame, $report); return Unknown)) end
 report!(frame::Frame, report::ErrorReport) = push!(frame.reports, report)
 
 macro maybe_report_argnumerr!(f, expected, actual)
+  profiled = gensym(:profiled)
   quote
-    if $expected !== (profiled = $actual isa Int ? $actual : length($actual))
-      @report!(ArgumentNumberErrorReport(frame, $f, $expected, profiled))
+    $profiled = $actual isa Int ? $actual : length($actual)
+    if $expected !== $profiled
+      @report!(ArgumentNumberErrorReport(frame, $f, $expected, $profiled))
     end
   end |> esc
 end
@@ -33,14 +30,10 @@ end
 
 function profile_subtype_call!(frame, argtyps)
   @maybe_report_argnumerr!(<:, 2, argtyps)
-
   expected = Tuple{Type,Type}
-  actual = Tuple[argtyps...]
-  if actual <: expected
-    return Bool
-  else
-    @report!(ArgumentTypeErrorReport(frame, <:, expected, actual))
-  end
+  actual = Tuple[argtyps[1], argtyps[2]]
+  actual <: expected && return Bool
+  @report!(ArgumentTypeErrorReport(frame, <:, expected, actual))
 end
 
 function profile_equiv_call!(frame, argtyps)
@@ -50,36 +43,25 @@ end
 
 function profile_ifelse_call!(frame, argtyps)
   @maybe_report_argnumerr!(ifelse, 3, argtyps)
-  condtyp, l, r  = @inbounds argtyps
-  if condtyp == Bool
-    return tmerge(l, r)
-  else
-    @report!(ArgumentTypeErrorReport(frame, ifelse, Bool, condtyp))
-  end
+  condtyp, l, r  = argtyps
+  condtyp == Bool && return tmerge(l, r)
+  @report!(ArgumentTypeErrorReport(frame, ifelse, Bool, condtyp))
 end
 
 function profile_isa_call!(frame, argtyps)
   @maybe_report_argnumerr!(isa, 2, argtyps)
-
   expected = Tuple{Any,Type}
-  actual = Tuple{argtyps...}
-  if actual <: expected
-    return Bool
-  else
-    @report!(ArgumentTypeErrorReport(frame, isa, expected, actual))
-  end
+  actual = Tuple{argtyps[1], argtyps[2]}
+  actual <: expected && return Bool
+  @report!(ArgumentTypeErrorReport(frame, isa, expected, actual))
 end
 
 function profile_isdefined_call!(frame, argtyps)
   @maybe_report_argnumerr!(isdefined, 2, argtyps)
-
-  expected = Tuple{Any, Union{Symbol, Int}}
-  actual = Tuple{argtyps...}
-  if actual <: expected
-    return Bool
-  else
-    @report!(ArgumentTypeErrorReport(frame, isdefined, expected, actual))
-  end
+  expected = Tuple{Any, Union{Symbol,Int}}
+  actual = Tuple{argtyps[1], argtyps[2]}
+  actual <: expected && return Bool
+  @report!(ArgumentTypeErrorReport(frame, isdefined, expected, actual))
 end
 
 function profile_typeof_call!(frame, argtyps)
@@ -91,6 +73,5 @@ function profile_gotoifnot!(frame, gotoifnot_ex)
   # just check the node is really `Bool` type
   @return_if_unknown! condtyp = lookup_type(frame, gotoifnot_ex.args[1])
   condtyp == Bool && return condtyp
-
   @report!(ConditionErrorReport(frame, condtyp))
 end
