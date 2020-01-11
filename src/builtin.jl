f0f80f9eec58e3755182ae8e3aa18b08cce308ab
aviatesk <aviatesk@gmail.com>
2020-01-12 05:20:21 +0900

    update maybe_profile_builtin_call! doc

--- src/builtin.jl
+++ src/builtin.jl
@@ -1,20 +1,21 @@
 """
-    ret = maybe_profile_builtin_call!(frame, call_ex, expand::Bool)
+    ret = maybe_profile_builtin_call!(frame, call_ex)
 
-If `ftyp` is a builtin function typ, profile it and return its return type.
-Otherwise, return `call_argtypes` that represents types of the (non-builtin) call.
+If `call_ex` is a builtin call, profile it and return its return type,
+  otherwise, return `call_argtypes` that represents call argument types of the
+  (non-builtin) call.
 
-For this function to work, `frame.src` _**should**_ hold typed IR.
-Then a builtin call should already get through the abstract interpretation by
-  [`Core.Compiler.abstract_call_known`](@ref), and so let's just trust its result
-  and report an error if the return type is [`Union{}`](@ref)
+For this function to work, `frame.src` _**should**_ be a typed IR.
+Then a builtin call has already been through the abstract interpretation by
+  [`Core.Compiler.abstract_call_known`](@ref), and then we can trust its
+  result and report an error if the return type is [`Union{}`](@ref).
 
 !!! warning
-    For [`Core.IntrinsicFunction`]s, [`Core.Compiler.builtin_tfunction`](@ref) only
+    For `Core.IntrinsicFunction`s, [`Core.Compiler.builtin_tfunction`](@ref) only
       performs really rough estimation of its return type.
     Accordingly this function also can mis-profile errors in intrinsic function calls.
 """
-function maybe_profile_builtin_call!(frame, call_ex, expand::Bool = false)
+function maybe_profile_builtin_call!(frame, call_ex)
   @return_if_unknown! call_argtypes = collect_call_argtypes(frame, call_ex)
 
   ftyp = @inbounds call_argtypes[1]
