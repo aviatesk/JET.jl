315122ddd239db486a71c22b198a3783fe5a4a45
Shuhei Kadowaki <aviatesk@gmail.com>
2020-09-06 14:48:18 +0900

    special case `return_type_tfunc`

--- src/tfuncs.jl
+++ src/tfuncs.jl
@@ -20,3 +20,19 @@ function builtin_tfunction(interp::TPInterpreter, @nospecialize(f), argtypes::Ar
 
     return ret
 end
+
+# `return_type_tfunc` internally uses `abstract_call` to model `return_type` function; while
+# TP will reports it as an error if the `abstract_call` is invalid (e.g. no method matched),
+# `return_type` just returns `Union{}` (and thus `return_type_tfunc` returns `Const(Union{})`
+# in that case, so we need to special case this so that we just pass `NativeInterpreter` to
+# `abstract_call` and just do simple error check for argument numbers
+function return_type_tfunc(interp::TPInterpreter, argtypes::Vector{Any}, sv::InferenceState)
+    if length(argtypes) !== 3
+        # invalid argument number, let's report and return error result (i.e. `Bottom`)
+        add_remark!(interp, sv, NoMethodErrorReport(interp, sv, false))
+        return Bottom
+    else
+        # don't recursively pass on `TPInterpreter` via `invoke_native`
+        return return_type_tfunc(interp.native, argtypes, sv)
+    end
+end
