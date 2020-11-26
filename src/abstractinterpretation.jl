6db2419183da299fd07f1abb2f24a2526c696ef3
Shuhei Kadowaki <aviatesk@gmail.com>
2020-11-27 00:11:47 +0900

    overload abstract_call_method_with_const_args, sync report-throw away with local code cache retrieval

--- src/abstractinterpretation.jl
+++ src/abstractinterpretation.jl
@@ -26,7 +26,7 @@ end
     end
     push_inithook!(overload_abstract_call_gf_by_type!)
 
-the aim of this overloads are:
+the aims of this overload are:
 1. report `NoMethodErrorReport` on empty method signature matching
 2. keep inference on non-concrete call sites in toplevel frame created by [`virtual_process!`](@ref)
 3. don't bail out even after the current return type grows up to `Any` and collect as much
@@ -268,6 +268,133 @@ function is_empty_match(info)
     isa(res, MethodLookupResult) || return false # when does this happen ?
     return isempty(res.matches)
 end
+
+"""
+    function overload_abstract_call_method_with_const_args!()
+        ...
+    end
+    push_inithook!(overload_abstract_call_method_with_const_args!)
+
+the aim of this overloads is:
+1. force constant prop' even if the inference result can't be improved anymore when `rettype`
+   is already `Const`; this is because constant prop' can still produce more "correct"
+   analysis by throwing away the error reports in the callee frames
+2. when local cache for constant analysis is hit, reconstruct the "after-constant-prop' analysis"
+   state using `JETInterpreter`'s local report caches
+"""
+function overload_abstract_call_method_with_const_args!()
+
+# %% for easier interactive update of abstract_call_method_with_const_args
+Core.eval(CC, quote
+
+function abstract_call_method_with_const_args(interp::$(JETInterpreter), @nospecialize(rettype), @nospecialize(f), argtypes::Vector{Any}, match::MethodMatch, sv::InferenceState, edgecycle::Bool)
+    method = match.method
+    nargs::Int = method.nargs
+    method.isva && (nargs -= 1)
+    length(argtypes) >= nargs || return Any
+    haveconst = false
+    allconst = true
+    # see if any or all of the arguments are constant and propagating constants may be worthwhile
+    for a in argtypes
+        a = widenconditional(a)
+        if allconst && !isa(a, Const) && !isconstType(a) && !isa(a, PartialStruct)
+            allconst = false
+        end
+        if !haveconst && has_nontrivial_const_info(a) && const_prop_profitable(a)
+            haveconst = true
+        end
+        if haveconst && !allconst
+            break
+        end
+    end
+    # force constant propagation even if it doesn't improve return type;
+    # constant prop' may improve report accuracy
+    haveconst || #= improvable_via_constant_propagation(rettype) || =# return Any
+    if nargs > 1
+        if istopfunction(f, :getindex) || istopfunction(f, :setindex!)
+            arrty = argtypes[2]
+            # don't propagate constant index into indexing of non-constant array
+            if arrty isa Type && arrty <: AbstractArray && !issingletontype(arrty)
+                return Any
+            elseif arrty ⊑ Array
+                return Any
+            end
+        elseif istopfunction(f, :iterate)
+            itrty = argtypes[2]
+            if itrty ⊑ Array
+                return Any
+            end
+        end
+    end
+    if !allconst && (istopfunction(f, :+) || istopfunction(f, :-) || istopfunction(f, :*) ||
+                     istopfunction(f, :(==)) || istopfunction(f, :!=) ||
+                     istopfunction(f, :<=) || istopfunction(f, :>=) || istopfunction(f, :<) || istopfunction(f, :>) ||
+                     istopfunction(f, :<<) || istopfunction(f, :>>))
+        return Any
+    end
+    force_inference = allconst || InferenceParams(interp).aggressive_constant_propagation
+    if istopfunction(f, :getproperty) || istopfunction(f, :setproperty!)
+        force_inference = true
+    end
+    mi = specialize_method(match, !force_inference)
+    mi === nothing && return Any
+    mi = mi::MethodInstance
+    # decide if it's likely to be worthwhile
+    if !force_inference && !const_prop_heuristic(interp, method, mi)
+        return Any
+    end
+    inf_cache = get_inference_cache(interp)
+    inf_result = cache_lookup(mi, argtypes, inf_cache)
+    if inf_result === nothing
+        if edgecycle
+            # if there might be a cycle, check to make sure we don't end up
+            # calling ourselves here.
+            infstate = sv
+            cyclei = 0
+            while !(infstate === nothing)
+                if method === infstate.linfo.def && any(infstate.result.overridden_by_const)
+                    return Any
+                end
+                if cyclei < length(infstate.callers_in_cycle)
+                    cyclei += 1
+                    infstate = infstate.callers_in_cycle[cyclei]
+                else
+                    cyclei = 0
+                    infstate = infstate.parent
+                end
+            end
+        end
+        inf_result = InferenceResult(mi, argtypes)
+        frame = InferenceState(inf_result, #=cache=#false, interp)
+        frame === nothing && return Any # this is probably a bad generated function (unsound), but just ignore it
+        frame.limited = true
+        frame.parent = sv
+        push!(inf_cache, inf_result)
+        typeinf(interp, frame) || return Any
+    else
+        # local cache for this constant analysis is hit
+        inf_result = inf_result::InferenceResult
+        if !isa(inf_result.result, InferenceState)
+            linfo = inf_result.linfo
+            # corresponds to report throw away logic in `_typeinf(interp::JETInterpreter, frame::InferenceState)`
+            $(filter!)(r -> $(∉)(linfo, r.lineage), interp.reports)
+
+            # TODO: restore local cache
+            # caches = interp.cache[linfo] # local cache for this constant analysis should exist
+        end
+    end
+    result = inf_result.result
+    # if constant inference hits a cycle, just bail out
+    isa(result, InferenceState) && return Any
+    add_backedge!(inf_result.linfo, sv)
+    return result
+end
+
+end) # Core.eval(CC, quote
+# %% for easier interactive update of abstract_call_method_with_const_args
+
+end # function overload_abstract_call_method_with_const_args!()
+push_inithook!(overload_abstract_call_method_with_const_args!)
 
 function CC.abstract_eval_special_value(interp::JETInterpreter, @nospecialize(e), vtypes::VarTable, sv::InferenceState)
     ret = @invoke abstract_eval_special_value(interp::AbstractInterpreter, e, vtypes::VarTable, sv::InferenceState)
