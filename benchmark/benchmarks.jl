1360c64dd287644ac0ef0ec957a45bf38837afb4
Shuhei Kadowaki <aviatesk@gmail.com>
2020-12-13 16:41:56 +0900

    dummy benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -94,20 +94,21 @@ end
 
 using BenchmarkTools, .JETBenchmarkTools
 
-BenchmarkTools.DEFAULT_PARAMETERS.seconds = 60
+# BenchmarkTools.DEFAULT_PARAMETERS.seconds = 60
 
 const SUITE = BenchmarkGroup()
 
-SUITE["identity(nothing) (first time)"] = @jetbenchmarkable (@profile_call identity(nothing)) setup = (using JET)
-SUITE["sum(\"julia\")"] = @jetbenchmarkable (@profile_call sum("julia")) setup = begin
-    using JET
-    @profile_call identity(nothing)
-end
-SUITE["sum(\"julia\") (cached)"] = @jetbenchmarkable (@profile_call sum("julia")) setup = begin
-    using JET
-    @profile_call sum("julia")
-end
-SUITE["rand(Bool)"] = @jetbenchmarkable (@profile_call rand(Bool)) setup = begin
-    using JET
-    @profile_call identity(nothing)
-end
+# SUITE["identity(nothing) (first time)"] = @jetbenchmarkable (@profile_call identity(nothing)) setup = (using JET)
+# SUITE["sum(\"julia\")"] = @jetbenchmarkable (@profile_call sum("julia")) setup = begin
+#     using JET
+#     @profile_call identity(nothing)
+# end
+# SUITE["sum(\"julia\") (cached)"] = @jetbenchmarkable (@profile_call sum("julia")) setup = begin
+#     using JET
+#     @profile_call sum("julia")
+# end
+# SUITE["rand(Bool)"] = @jetbenchmarkable (@profile_call rand(Bool)) setup = begin
+#     using JET
+#     @profile_call identity(nothing)
+# end
+SUITE["test"] = @jetbenchmarkable identity(:nothing) seconds = 10
