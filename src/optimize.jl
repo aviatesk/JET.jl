b8263dddf081b648d0e2bcaae6c798ccb02a8004
Shuhei Kadowaki <aviatesk@gmail.com>
2021-02-18 02:03:24 +0900

    update dated note on `optimize`

--- src/optimize.jl
+++ src/optimize.jl
@@ -3,11 +3,11 @@ function CC.OptimizationState(linfo::MethodInstance, params::OptimizationParams,
 end
 
 function CC.optimize(interp::JETInterpreter, opt::OptimizationState, params::OptimizationParams, result)
-    # NOTE: to keep the previous implementations as a future reference
+    # NOTE: don't recurse with `@invoke`
     # `JETInterpreter` shouldn't recur into the optimization step via `@invoke` macro,
     # but rather it should just go through `NativeInterpreter`'s optimization pass
     # this is necessary because `CC.get(wvc::WorldView{JETCache}, mi::MethodInstance, default)`
     # can be called from optimization pass otherwise, and it may cause errors because our report
-    # construction is only valid before optimization pass
+    # construction is only valid on inference frames before the optimizer runs on it
     return optimize(interp.native, opt, params, result)
 end
