0879b1fb5be9952dbc63bd3a8ee42904dcd3d6ea
Shuhei Kadowaki <aviatesk@gmail.com>
2021-06-03 00:28:45 +0900

    wip: include plugins documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,62 @@
 using Documenter, JET
 
+joinlines(lines) = join(lines, '\n')
+
+function make_example_doc(filename)
+    doc, code = get_doc_and_code(filename)
+    return joinlines((
+        doc,
+        "```julia",
+        code,
+        "```",
+    ))
+end
+
+function get_doc_and_code(filename)
+    doclines, codelines = String[], String[]
+    indoc = false
+    for line in readlines(filename)
+        if line == "\"\"\""
+            indoc = !indoc
+            continue
+        end
+        push!(indoc ? doclines : codelines, line)
+    end
+    return joinlines(doclines), joinlines(codelines)
+end
+
+function attach_doc!(dir)
+    docs = String[]
+
+    function gen_doc!(dir)
+        for (root, dirs, files) in walkdir(dir)
+            for file in files
+                endswith(file, ".jl") || continue
+                push!(docs, make_example_doc(normpath(root, file)))
+            end
+            for dir in dirs
+                gen_doc!(normpath(root, dir))
+            end
+        end
+    end
+    gen_doc!(dir)
+
+    doc = join(docs, "\n---\n")
+
+    @eval JET module PluginExamples end
+    @eval JET @doc $doc PluginExamples
+end
+
+attach_doc!(normpath(@__DIR__, "..", "examples"))
+
 makedocs(; modules = [JET],
            sitename="JET.jl",
            pages = [
-             "README" => "index.md",
-             "Usages" => "usages.md",
-             "Configurations" => "config.md",
-             "Internals" => "internals.md",
+                "README" => "index.md",
+                "Usages" => "usages.md",
+                "Configurations" => "config.md",
+                "Internals" => "internals.md",
+                "Pluggable Analysis Framework" => "plugins.md",
            ],
            format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
            )
---
+++ docs/src/plugins.md
@@ -0,0 +1,7 @@
+# JET.jl Pluggable Analysis Framework
+
+The documentation for the framework is very WIP.
+
+```@docs
+JET.PluginExamples
+```
