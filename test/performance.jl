1a6a640ea0cb7e04dc835ba0213e6a84c806ce12
Shuhei Kadowaki <aviatesk@gmail.com>
2021-09-09 01:31:24 +0900

    allow CI test runner to be a bit more slow

--- test/performance.jl
+++ test/performance.jl
@@ -1,4 +1,4 @@
 @testset "https://github.com/aviatesk/JET.jl/issues/71" begin
     stats = @benchmark_freshexec ntimes = 1 @report_call println(QuoteNode(nothing))
-    @test stats.time ≤ 10
+    @test stats.time ≤ (get(ENV, "CI", nothing) == "true" ? 20 : 10) # a CI runner might be slow
 end
