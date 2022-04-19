4b42a8e23988a3bf0842d155679bf5e0e534b70a
Shuhei Kadowaki <aviatesk@gmail.com>
2022-04-19 11:53:43 +0900

    make better use of `JETBenchmarkUtils` in the test suite

--- test/performance.jl
+++ test/performance.jl
@@ -3,6 +3,28 @@ include(normpath(@__DIR__, "..", "benchmark", "JETBenchmarkUtils.jl"))
 using .JETBenchmarkUtils
 
 @testset "https://github.com/aviatesk/JET.jl/issues/71" begin
-    stats = @benchmark_freshexec ntimes = 1 @report_call println(QuoteNode(nothing))
-    @test stats.time ≤ (get(ENV, "CI", nothing) == "true" ? 20 : 10) # a CI runner might be slow
+    let ok = false
+        for _ = 1:3 # try multiple times
+            stats = @timed @report_call report_pass=FreshPass(JET.BasicPass()) println(QuoteNode(nothing))
+            if stats.time ≤ (get(ENV, "CI", nothing) == "true" ? 20 : 10) # a CI runner might be slow
+                ok = true
+                break
+            else
+                @warn "bad performance detected: @report_call report_pass=FreshPass(JET.BasicPass()) println(QuoteNode(nothing))"
+            end
+        end
+        @test ok
+    end
+    let ok = false
+        for _ = 1:3 # try multiple times
+            stats = @timed @report_call report_pass=FreshPass(JET.SoundPass()) println(QuoteNode(nothing))
+            if stats.time ≤ (get(ENV, "CI", nothing) == "true" ? 20 : 10) # a CI runner might be slow
+                ok = true
+                break
+            else
+                @warn "bad performance detected: @report_call report_pass=FreshPass(JET.SoundPass()) println(QuoteNode(nothing))"
+            end
+        end
+        @test ok
+    end
 end
