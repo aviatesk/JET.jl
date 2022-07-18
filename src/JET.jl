9f2bab984365d70645541cec14a373f446027387
Sebastian Pfitzner <pfitzseb@gmail.com>
2022-07-18 12:04:23 +0200

    Fix invalid `@static` invokation

--- src/JET.jl
+++ src/JET.jl
@@ -677,7 +677,7 @@ end
 fullbasepath(filename) = normpath(JULIA_DIR, "base", filename)
 
 # TODO make this configurable ?
-const JULIA_DIR = @static begin
+const JULIA_DIR = begin
     p1 = normpath(Sys.BINDIR, "..", "..", "..", "julia")
     p2 = normpath(Sys.BINDIR, Base.DATAROOTDIR, "julia")
     ispath(p1) ? p1 : p2
