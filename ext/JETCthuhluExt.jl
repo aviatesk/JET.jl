4145028c7a06d225341c9b96cdbf4b6dd42b9a0f
Tim Holy <tim.holy@gmail.com>
2024-07-14 06:11:08 -0500

    Add Cthulhu.ascend integration
    Closes #511

---
+++ ext/JETCthuhluExt.jl
@@ -0,0 +1,25 @@
+module JETCthulhuExt
+
+using JET: JET, RuntimeDispatchReport, VirtualFrame
+using Cthulhu: Cthulhu, Node, Data, callstring
+using Core: MethodInstance
+
+const _emptybackedges = MethodInstance[]
+
+struct CallFrames
+    frames::Vector{VirtualFrame}
+end
+
+function Cthulhu.treelist(r::RuntimeDispatchReport)
+    io = IOBuffer()
+    cf = CallFrames(r.vst)
+    frame = r.vst[end]
+    str = callstring(io, frame.linfo)
+    Cthulhu.treelist!(Node(Data(str, frame.linfo)), io, cf, "", Base.IdSet{MethodInstance}())
+end
+
+Cthulhu.instance(cf::CallFrames) = cf.frames[end].linfo
+Cthulhu.backedges(cf::CallFrames) = isempty(cf.frames) ? _emptybackedges : [cf.frames[end].linfo]
+Cthulhu.nextnode(cf::CallFrames, ::MethodInstance) = CallFrames(cf.frames[1:end-1])
+
+end
