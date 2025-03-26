6e6660100e2428707894df9571e3763dcb510248
Shuhei Kadowaki <aviatesk@gmail.com>
2025-03-26 14:08:43 +0900

    fix up runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,10 +21,7 @@ isempty(errmsg) || println(errmsg)
 
 @info "JET setup information:" JET.JET_DEV_MODE
 
-# @static Sys.iswindows() || @testset "print.jl" include("ui/test_print.jl")
-@testset "JETInterface" include("test_JETInterface.jl")
-
-false && @testset "JET.jl" begin
+@testset "JET.jl" begin
     @static if JET.JET_LOADABLE
         @testset "abstractinterpret" begin
             @testset "inferenceerrorreport.jl" include("abstractinterpret/test_inferenceerrorreport.jl")
